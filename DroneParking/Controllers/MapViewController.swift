06e8867ada7b54ae116a9575c1c36de79cf7687d
Scott Corbat <scorbat@riis.com>
2021-07-28 14:42:07 -0400

    Add location focusing Button on map view focuses map to current location Add descriptions for location usage

--- DroneParking/Controllers/MapViewController.swift
+++ DroneParking/Controllers/MapViewController.swift
@@ -8,11 +8,13 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController, MKMapViewDelegate {
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     
     let mapService = MapService()
+    let locationManager = CLLocationManager()
+    var userLocation: CLLocationCoordinate2D?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,13 +22,55 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         //create tap gesture for map view
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(addWaypoint(tapGesture:)))
         mapView.addGestureRecognizer(tapGesture)
+        
+        //enable location updating
+        enableLocationTracking()
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        locationManager.stopUpdatingLocation()
+    }
+    
+    //MARK: - My methods
     
     @objc func addWaypoint(tapGesture: UITapGestureRecognizer) {
         if tapGesture.state == .ended {
             let point = tapGesture.location(in: mapView)
             mapService.add(point: point, for: mapView)
         }
+    }
+    
+    //focuses map to user's current location
+    func focusMap() {
+        guard let location = userLocation else {
+            print("User Location not valid!")
+            return
+        }
+        
+        if CLLocationCoordinate2DIsValid(location) {
+            let span = MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001)
+            let region = MKCoordinateRegion(center: location, span: span)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    func enableLocationTracking() {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.distanceFilter = 0.1
+            locationManager.requestAlwaysAuthorization()
+            locationManager.startUpdatingLocation()
+        } else {
+            showAlert(from: self, title: "Location Service", message: "Location is not enabled")
+        }
+    }
+    
+    //MARK: - IBActions
+    
+    @IBAction func focusPressed(_ sender: UIButton) {
+        focusMap()
     }
     
     //MARK: - MKMapViewDelegate methods
@@ -38,6 +82,12 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         }
         
         return nil
+    }
+    
+    //MARK: - CLLocationManagerDelegate methods
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        userLocation = locations.last?.coordinate
     }
 
 }
